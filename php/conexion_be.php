6dcb8e606e910ceab4b400f051d0a0b14b07e9e2
DeepLmon-git <johannecl07@gmail.com>
2024-04-27 15:39:58 -0500

    Update conexion_be.php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,7 +1,12 @@
 <?php   
 
     $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
-
+    
+    if($conexion){
+        echo 'conectado'
+    }else{
+        echo 'no conectado'
+    }
 
 
 
