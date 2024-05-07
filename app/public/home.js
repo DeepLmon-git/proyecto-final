a68f52865c9084220c4a585d1b79b32dc0da4efb
DeepLmon-git <johannecl07@gmail.com>
2024-05-07 18:49:15 -0500

    cambios

--- app/public/home.js
+++ app/public/home.js
@@ -1,18 +1,23 @@
-const trigger = document.getElementById('trigger')
-const header = document.getElementById('header')
-trigger.addEventListener('mouseover',function(){
-    header.classList.toggle("visible")
-    header.classList.toggle("visible2")
-})
+const trigger = document.getElementById('trigger');
+const header = document.getElementById('header');
 function hover(){
     var header= document.getElementById('header');
     var nav= document.getElementById('nav');
+    var flecha= document.getElementById('flecha');
+    var shadow= document.getElementById('shadow');
     header.classList.add('visible');
     nav.classList.add("visible2");
+    flecha.classList.add("flechaD");
+    shadow.classList.add("efectoShadow2");
+    
 }
 function disapear(){
     var header= document.getElementById('header');
     var nav= document.getElementById('nav');
+    var flecha= document.getElementById('flecha');
+    var shadow= document.getElementById('shadow');
     header.classList.remove('visible');
     nav.classList.remove("visible2");
+    flecha.classList.remove("flechaD");
+    shadow.classList.remove("efectoShadow2");
 }
