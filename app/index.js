f2e083081a2b3297ed53687035d432a4a9e9fd70
DeepLmon-git <johannecl07@gmail.com>
2024-05-27 17:38:01 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ import { methods as authorization } from "./middlewares/authorization.js";
 const app = express();
 
 
-app.set("port", process.env.PORT || 4000);
+//app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), ()=>{
     console.log(app.get("port"))
     app.use(express.static(__dirname + "/public"));
