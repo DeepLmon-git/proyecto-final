e683edb9ca60ff77a8b18f3f845599228ea82fca
DeepLmon-git <johannecl07@gmail.com>
2024-05-27 00:40:38 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -7,20 +7,18 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import { methods as authentication } from "./controllers/authentication.controllers.js";
 import { methods as authorization } from "./middlewares/authorization.js";
 const app = express();
-app.set("port", process.env.PORT);
-app.listen(app.get("port"), () => {
-    app.use(express.static(__dirname + "/public"));
-    app.use(express.json());
-    app.use(cookieParser());
+/*app.set("port", process.env.PORT);
+app.listen(app.get("port"));*/
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(cookieParser());
 
-    app.get("/", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/index.html"));
-    app.get("/login", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/login.html"));
-    app.get("/register", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/register.html"));
-    app.get("/admin", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/admin.html"));
-    app.get("/home", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/Home.html"));
-    app.post("/api/register", authentication.register);
-    app.post("/api/login", authentication.login);
-
-});
+app.get("/", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/login", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/login.html"));
+app.get("/register", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/register.html"));
+app.get("/admin", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/admin.html"));
+app.get("/home", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/Home.html"));
+app.post("/api/register", authentication.register);
+app.post("/api/login", authentication.login);
 
 //config
