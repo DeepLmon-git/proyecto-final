c90b8c8eb51a33bf2e183e04241a8c9d1c7bd004
DeepLmon-git <johannecl07@gmail.com>
2024-05-27 17:57:22 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -9,22 +9,21 @@ import { methods as authorization } from "./middlewares/authorization.js";
 const app = express();
 
 
-app.set("port", process.env.PORT || 4000);
-app.listen(app.get("port"), ()=>{
-    console.log(app.get("port"))
-    app.use(express.static(__dirname + "/public"));
-    app.use(express.json());
-    app.use(cookieParser());
-    
-    
-    app.get("/", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/index.html"));
-    app.get("/login", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/login.html"));
-    app.get("/register", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/register.html"));
-    app.get("/admin", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/admin.html"));
-    app.get("/home", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/Home.html"));
-    app.post("/api/register", authentication.register);
-    app.post("/api/login", authentication.login);
-});
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log(app.get("port"))
 
 
 //config
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/login", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/login.html"));
+app.get("/register", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/register.html"));
+app.get("/admin", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/admin.html"));
+app.get("/home", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/Home.html"));
+app.post("/api/register", authentication.register);
+app.post("/api/login", authentication.login);
