13ba3ba6f285b389ace184af61962a19c367d785
DeepLmon-git <johannecl07@gmail.com>
2024-05-26 23:45:44 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,23 +2,25 @@ import express, { json } from "express";
 import cookieParser from "cookie-parser";
 //fix para __dirname
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-import {methods as authentication} from "./controllers/authentication.controllers.js";
-import {methods as authorization} from "./middlewares/authorization.js";
-const app=express();
+import { methods as authentication } from "./controllers/authentication.controllers.js";
+import { methods as authorization } from "./middlewares/authorization.js";
+const app = express();
 app.set("port", process.env.PORT || 4000);
-app.listen(app.get("port"));
+app.listen(app.get("port"), () => {
+    app.use(express.static(__dirname + "/public"));
+    app.use(express.json());
+    app.use(cookieParser());
+
+    app.get("/", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/index.html"));
+    app.get("/login", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/login.html"));
+    app.get("/register", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/pags/register.html"));
+    app.get("/admin", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/admin.html"));
+    app.get("/home", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/pags/admin/Home.html"));
+    app.post("/api/register", authentication.register);
+    app.post("/api/login", authentication.login);
+
+});
 
 //config
-app.use(express.static(__dirname + "/public"));
-app.use(express.json());
-app.use(cookieParser());
-
-app.get("/",authorization.soloPublico,(req,res)=> res.sendFile(__dirname+"/index.html"));
-app.get("/login",authorization.soloPublico,(req,res)=> res.sendFile(__dirname+"/pags/login.html"));
-app.get("/register",authorization.soloPublico,(req,res)=> res.sendFile(__dirname+"/pags/register.html"));
-app.get("/admin",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname+"/pags/admin/admin.html"));
-app.get("/home",authorization.soloAdmin,(req,res)=> res.sendFile(__dirname+"/pags/admin/Home.html"));
-app.post("/api/register",authentication.register);
-app.post("/api/login",authentication.login);
