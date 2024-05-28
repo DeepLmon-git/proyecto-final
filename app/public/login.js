60e6d0fe6c82323dd01b2a4adb0fcc6aeff2264a
DeepLmon-git <johannecl07@gmail.com>
2024-05-28 01:58:29 -0500

    sds

--- app/public/login.js
+++ app/public/login.js
@@ -1,10 +1,11 @@
 const mensajeError = document.getElementsByClassName("error")[0]
 
+
 document.getElementById("login-form").addEventListener("submit",async (e)=>{
   e.preventDefault();
   const email = e.target.children.email.value;
   const password = e.target.children.password.value;
-  const res = await fetch("http://localhost:4000/api/login",{
+  const res = await fetch("http://localhost:3000/api/login",{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
---
+++ index.js
@@ -0,0 +1,33 @@
+import express, { json } from "express";
+import cookieParser from "cookie-parser";
+//fix para __dirname
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { methods as authentication } from "./app/controllers/authentication.controllers.js";
+import { methods as authorization } from "./app/middlewares/authorization.js";
+const app = express();
+const port = process.env.PORT || 3000;
+console.log(__dirname);
+
+//config
+app.use(express.static(__dirname + "/app/public"));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/app/index.html"));
+app.get("/login", authorization.soloPublico, (req, res) => {
+    res.sendFile(__dirname + "/app/pags/login.html")
+    
+});
+app.get("/register", authorization.soloPublico, (req, res) => res.sendFile(__dirname + "/app/pags/register.html"));
+app.get("/admin", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/app/pags/admin/admin.html"));
+app.get("/home", authorization.soloAdmin, (req, res) => res.sendFile(__dirname + "/app/pags/admin/Home.html"));
+app.post("/api/register", authentication.register);
+app.post("/api/login", authentication.login);
+
+app.listen(port, ()=>{
+    console.log("el puerto es " + port) 
+    
+});
