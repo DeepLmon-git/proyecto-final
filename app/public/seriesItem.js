456ad4a4a2d9371b861af8148078845947057d02
DeepLmon-git <johannecl07@gmail.com>
2024-06-02 15:44:04 -0500

    episodios responsive maqueta

--- app/public/seriesItem.js
+++ app/public/seriesItem.js
@@ -30,16 +30,19 @@ function disableButtons(state){
     const btnRecomendados = document.getElementById('botonR');
     const sectionEpisodios = document.querySelector('.episodios');
     const sectionRecomendados = document.querySelector('.recomendados');
+    const footer = document.getElementById('footer');
     btnEpisodios.addEventListener('click',()=>{
         sectionRecomendados.classList.remove('visible3');
         sectionEpisodios.classList.add('visible3');
         btnEpisodios.classList.add('activo');
         btnRecomendados.classList.remove('activo');
+        footer.classList.remove('footer2');
     })
     btnRecomendados.addEventListener('click',()=>{
         sectionEpisodios.classList.remove('visible3');
         sectionRecomendados.classList.add('visible3');
         btnRecomendados.classList.add('activo');
+        footer.classList.add('footer2');
         btnEpisodios.classList.remove('activo');
     })
 })
